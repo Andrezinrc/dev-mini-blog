518176cbc6f7e37369d36f84109ae0b3d025b034
Andrezinrc <andrem.33333@gmail.com>
2025-11-26 03:02:42 +0000

    fix: Corrigido layout nao exportado

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export {default as Header} from './layout/Header/Header';
-export {default as Footer} from './layout/Footer/Footer';
+export {default as Layout} from './layout/Layout';
 export {default as Load} from './layout/Load/Load';
 export {default as ScrollToTop} from './layout/ScrollToTop/ScrollToTop';
